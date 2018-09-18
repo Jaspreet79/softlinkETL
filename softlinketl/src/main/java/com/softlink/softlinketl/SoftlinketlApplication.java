33a2ec322d84629536e46b0d8e6a7df2368bc5d1
jaspreet-singh <jaspreet79@gmail.com>
2018-09-18 13:42:39 +0530

    softlink ETL project source directory directory

---
+++ softlinketl/src/main/java/com/softlink/softlinketl/SoftlinketlApplication.java
@@ -0,0 +1,12 @@
+package com.softlink.softlinketl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftlinketlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftlinketlApplication.class, args);
+    }
+}
---
+++ softlinketl/src/test/java/com/softlink/softlinketl/SoftlinketlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.softlink.softlinketl;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoftlinketlApplicationTests {
+
+    @Ignore
+    public void contextLoads() {
+    }
+
+}
